a041f1f0880156dfc3d95c7a9da61bcd1a1d2ad0
fish214057 <2140579318@qq.com>
2022-05-09 17:50:12 +0800

    Site updated: 2022-05-09 17:50:11

---
+++ js/weather.js
@@ -0,0 +1,23 @@
+WIDGET = {
+    "CONFIG": {
+        "modules": "10243",
+        "background": "5",
+        "tmpColor": "DEA9DE",
+        "tmpSize": "16",
+        "cityColor": "FFF2CC",
+        "citySize": "16",
+        "aqiColor": "5FFAFA",
+        "aqiSize": "16",
+        "weatherIconSize": "24",
+        "alertIconSize": "18",
+        "padding": "10px 10px 10px 10px",
+        "shadow": "0",
+        "language": "auto",
+        "fixed": "true",
+        "vertical": "center",
+        "horizontal": "center",
+        "left": "100",
+        "top": "8",
+        "key": "b52ed6413a154d5b959900da12236b33"
+    }
+}
